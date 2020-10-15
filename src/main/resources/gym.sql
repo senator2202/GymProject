613a4499189f02e3249a64053818311df62c586d
senator2202 <Nuttertools220291>
2020-10-16 00:04:09 +0300

    coding task

--- src/main/resources/gym.sql
+++ src/main/resources/gym.sql
@@ -68,7 +68,7 @@ REPLACE INTO `accounts` (`account_id`, `login`, `password`, `email`, `role`, `re
 	(281, 'client5052467', 'AQXR9Uqgzh7g~JKg', 'email5591607@gmail.com', 'CLIENT', '2020-09-09 21:29:37', 'RUSSIAN', 0),
 	(282, 'client4738890', 'AEXQ6Uqgzh7g~JKg', 'email8151779@gmail.com', 'CLIENT', '2020-09-09 21:29:37', 'RUSSIAN', 0),
 	(283, 'client289959', 'Ak~R7Uqgzh7g~JKg', 'email794059@gmail.com', 'CLIENT', '2020-09-09 21:29:37', 'RUSSIAN', 1),
-	(284, 'senator2202', 'AAER~MXQ9Vrgph\\g', 'senator220291@gmail.com', 'CLIENT', '2020-09-10 10:17:40', 'RUSSIAN', 1),
+	(284, 'senator2202', 'AAER~MXQ9Vrgph\\g', 'senator220291@gmail.com', 'CLIENT', '2020-09-10 10:17:40', 'ENGLISH', 1),
 	(285, 'client3826502', 'A]~Q}Uqgzh7g~JKg', 'email2463376@gmail.com', 'CLIENT', '2020-09-17 09:11:08', 'RUSSIAN', 0),
 	(286, 'client9528772', 'AIXQ|Uqgzh7g~JKg', 'email1443461@gmail.com', 'CLIENT', '2020-09-17 09:11:08', 'RUSSIAN', 0),
 	(288, 'abra', '9Vrgph\\g', 'abracadabra@gmail.com', 'CLIENT', '2020-09-17 12:06:52', 'RUSSIAN', 1),
@@ -172,21 +172,19 @@ CREATE TABLE IF NOT EXISTS `trainings` (
   KEY `FK_trainings_users_2` (`client_id`),
   CONSTRAINT `FK_trainings_users` FOREIGN KEY (`trainer_id`) REFERENCES `users` (`user_id`),
   CONSTRAINT `FK_trainings_users_2` FOREIGN KEY (`client_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
 
 -- Дамп данных таблицы gym.trainings: ~10 rows (приблизительно)
 /*!40000 ALTER TABLE `trainings` DISABLE KEYS */;
 REPLACE INTO `trainings` (`training_id`, `trainer_id`, `client_id`, `training_date`, `training_time`, `done`) VALUES
-	(1, 274, 254, '2020-09-20', '16:00:00', NULL),
-	(2, 301, 255, '2020-09-20', '18:00:00', NULL),
-	(3, 302, 260, '2020-09-18', '12:00:00', NULL),
 	(9, 301, 284, '2020-10-15', '12:00:00', 0),
 	(10, 302, 284, '2020-10-16', '12:00:00', 0),
-	(12, 301, 284, '2020-10-23', '12:00:00', 0),
+	(12, 301, 288, '2020-10-23', '12:00:00', 0),
 	(13, 301, 284, '2020-10-24', '12:00:00', 0),
-	(14, 301, 284, '2020-10-25', '12:00:00', 0),
+	(14, 301, 303, '2020-10-25', '12:00:00', 0),
 	(15, 301, 284, '2020-10-26', '12:00:00', 0),
-	(16, 274, 284, '2020-10-21', '12:00:00', 0);
+	(16, 274, 284, '2020-10-21', '12:00:00', 0),
+	(17, 274, 284, '2020-10-30', '12:00:00', 0);
 /*!40000 ALTER TABLE `trainings` ENABLE KEYS */;
 
 -- Дамп структуры для таблица gym.training_exercises
@@ -202,11 +200,11 @@ CREATE TABLE IF NOT EXISTS `training_exercises` (
 -- Дамп данных таблицы gym.training_exercises: ~5 rows (приблизительно)
 /*!40000 ALTER TABLE `training_exercises` DISABLE KEYS */;
 REPLACE INTO `training_exercises` (`training_id_fk`, `type`, `weight`) VALUES
-	(1, 'BENCH_PRESS', 100),
-	(1, 'DEADLIFT', 150),
-	(1, 'SQUATS', 140),
-	(2, 'BENCH_PRESS', 60),
-	(2, 'SQUATS', 100);
+	(9, 'BENCH_PRESS', 100),
+	(9, 'DEADLIFT', 150),
+	(9, 'SQUATS', 140),
+	(10, 'BENCH_PRESS', 60),
+	(10, 'SQUATS', 100);
 /*!40000 ALTER TABLE `training_exercises` ENABLE KEYS */;
 
 -- Дамп структуры для таблица gym.users
@@ -266,7 +264,7 @@ REPLACE INTO `users` (`user_id`, `first_name`, `last_name`, `phone`, `rating`, `
 	(281, '', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 	(282, '', '', '', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
 	(283, '', '', '', NULL, NULL, NULL, NULL, NULL, 1, NULL, NULL, NULL),
-	(284, 'Angela', 'Merkel', '+375335678962', NULL, NULL, NULL, NULL, 10, 1, NULL, 1000, 4),
+	(284, 'Angela', 'Merkel', '+375335678962', NULL, NULL, NULL, NULL, 10, 1, NULL, 640, 23),
 	(285, '', '', '', 0, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL),
 	(286, '', '', '', 0, NULL, NULL, NULL, 0, NULL, NULL, NULL, NULL),
 	(288, 'Chef', 'Ivlev', '80295554466', 0, NULL, NULL, NULL, 0, 2, NULL, NULL, NULL),
